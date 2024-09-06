478dea98bb53e9c9933f80df78dff173f78a0374
Samir Neto <samihneto@gmail.com>
2024-09-05 22:49:18 -0300

    Criação do Register e arrumando header e footer nas páginas. Ligando as rotas.
    Co-authored-by: André M. <andremarko@users.noreply.github.com>
    Co-authored-by: Felipe_Fidelix <felipe-2833@users.noreply.github.com>

--- challenge1-front/src/routes/index.tsx
+++ challenge1-front/src/routes/index.tsx
@@ -3,6 +3,7 @@ import Home from "../pages/home";
 import About from "../pages/about";
 import Agenda from "../pages/agenda";
 import Mecv from "../pages/mv";
+import Register from "../pages/register";
 
 
 export default function RoutesMain() {
@@ -12,6 +13,7 @@ export default function RoutesMain() {
             <Route Component={ About } path="/about" />
             <Route Component={ Agenda } path="/agenda" />
             <Route Component={ Mecv } path="/mv" />
+            <Route Component={ Register } path="/register" />
         </Routes>
     )
 }
