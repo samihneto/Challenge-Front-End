492c30681f9665de8010eaa84fccee847593f443
Samir Neto <samihneto@gmail.com>
2024-09-06 06:27:34 -0300

    Atualizações menores

--- challenge1-front/src/components/Header.tsx
+++ challenge1-front/src/components/Header.tsx
@@ -8,10 +8,10 @@ export default function Header() {
             <img className="logo" src={logoMV} alt="Logotipo do Mecânico Virtual" />
             <nav className="navegacao">
                 <ul className="paginas">
-                    <li><a className="link" href="">HOME</a></li>
-                    <li><a className="link" href="./pages/mv.html">MV</a></li>
-                    <li><a className="link" href="./pages/agenda.html">AGENDAR</a></li>
-                    <li><a className="link" href="./pages/about.html">SOBRE NÓS</a></li>
+                    <li><a className="link" href="/">HOME</a></li>
+                    <li><a className="link" href="/mv">MV</a></li>
+                    <li><a className="link" href="/agenda">AGENDAR</a></li>
+                    <li><a className="link" href="/about">SOBRE NÓS</a></li>
                 </ul>
             </nav>
             <input type="text" className="procurar" placeholder="O quê você procura?" />
--- challenge1-front/src/pages/login.tsx
+++ challenge1-front/src/pages/login.tsx
@@ -1,6 +1,6 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header"
-import "../login.css"
+import "../styles/login.css"
 
 export default function Login() {
     return (
