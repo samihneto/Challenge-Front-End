b361382ef51780e2029079e3ce067ecd7913e6a5
Samir Neto <samihneto@gmail.com>
2024-09-05 22:44:53 -0300

    Criação e adaptação da página do Mecânico Virtual com estabelecimento da rota.
    Co-authored-by: André M. <andremarko@users.noreply.github.com>
    Co-authored-by: Felipe_Fidelix <felipe-2833@users.noreply.github.com>

---
+++ challenge1-front/src/pages/mv.tsx
@@ -0,0 +1,35 @@
+
+import "../styles/mv.css";
+import ferramentaImg from "../img/content/mv/tools.png";
+import cameraIcon from "../img/content/mv/camera.png";
+import fileIcon from "../img/content/mv/file.png";
+
+export default function Mecv() {
+    return (
+        <div>
+             <main className="mv">
+            <div className="parte_superior">
+                <img className="mv__ferramenta" src={ferramentaImg} alt="Ilustração de uma ferramenta" />
+                <h1 className="mv__titulo">Mecânico Virtual</h1>
+            </div>
+            <section className="bot_box">
+                <p className="txt__bot">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nunc lectus, pharetra scelerisque suscipit sed, pellentesque at metus. Proin in sapien sed quam egestas vestibulum tempor gravida elit. Curabitur arcu enim, placerat eget ex at, porttitor commodo urna. Duis ex diam, pretium eget pulvinar et, accumsan at velit. Vestibulum gravida ultrices tincidunt. Morbi ac sem diam. Etiam rutrum felis sed leo finibus imperdiet. Maecenas lacus magna, egestas non vehicula ac, hendrerit at ex. Donec blandit auctor arcu. Suspendisse mattis nibh sed pharetra commodo. Cras quis elit at ante elementum sodales. Donec consequat faucibus convallis.
+                </p>
+                <p className="txt__user">
+                    Donec consequat faucibus convallis. Vivamus quam justo, vulputate in diam nec, cursus commodo ipsum. Ut lobortis vulputate libero vitae aliquam. Morbi porttitor consequat malesuada.
+                </p>
+                <div className="bot__chat">
+                    <input type="text" className="bot__input" placeholder="Digite sua mensagem..." />
+                    <a className="bot__btn">
+                        <img src={cameraIcon} alt="Ícone câmera" />
+                    </a>
+                    <a className="bot__btn">
+                        <img src={fileIcon} alt="Ícone arquivo" />
+                    </a>
+                </div>
+            </section>
+        </main>
+        </div>
+    )
+}
--- challenge1-front/src/routes/index.tsx
+++ challenge1-front/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/home";
 import About from "../pages/about";
 import Agenda from "../pages/agenda";
+import Mecv from "../pages/mv";
 
 
 export default function RoutesMain() {
@@ -10,6 +11,7 @@ export default function RoutesMain() {
             <Route Component={ Home } path="/" />
             <Route Component={ About } path="/about" />
             <Route Component={ Agenda } path="/agenda" />
+            <Route Component={ Mecv } path="/mv" />
         </Routes>
     )
 }
