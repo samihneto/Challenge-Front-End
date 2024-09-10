013f00794701993de703d9ac497fbb69ffa7803a
Samir Neto <samihneto@gmail.com>
2024-09-09 22:24:12 -0300

    Arrumando links
    Co-authored-by: André M. <andremarko@users.noreply.github.com>
    Co-authored-by: Felipe_Fidelix <felipe-2833@users.noreply.github.com>

--- challenge1-front/src/components/Entry/Entry.tsx
+++ challenge1-front/src/components/Entry/Entry.tsx
@@ -3,9 +3,11 @@ import styles from './styles.module.css'
 export default function Entry() {
     return (
         <main className={styles.entry__conteudo}>
-            <button className={styles.botao__login}>Login</button>
+            <button className={styles.botao__login}>Login<link rel="stylesheet" href="/register" /></button>
             <h1>-ou-</h1>
-            <button className={styles.botao__cadastro}>Cadastro</button>
+            <button className={styles.botao__cadastro}> Cadastro
+                <link rel="stylesheet" href="/register" />
+            </button>
         </main>
     )
 }
---
+++ challenge1-front/src/pages/entrada.tsx
@@ -0,0 +1,16 @@
+import Entry from "../components/Entry/Entry"
+import Footer from "../components/Footer/Footer"
+import Header from "../components/Header/Header"
+import News from '../components/News/News'
+
+export default function Entrada() {
+    return (
+        <div>
+            <Header />
+            <Entry />
+            <News />
+            <Footer />
+        </div>
+    )
+}
+
