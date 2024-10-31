8c0e3a61da940b54174467b800e1d54e60c50bdb
Samir Neto <samihneto@gmail.com>
2024-10-31 20:27:52 -0300

    Criação de uma página Not-Found.
    Co-authored-by: André M. <andremarko@users.noreply.github.com>
    Co-authored-by: Felipe_Fidelix <felipe-2833@users.noreply.github.com>

--- challenge-1tdspg/src/app/not-found.tsx
+++ challenge-1tdspg/src/app/not-found.tsx
@@ -5,9 +5,9 @@ import erro from "@/public/imgs/content/not-found/erro.png";
 export default async function NotFound() {
     
     return (
-        <div>
-            <h1> Erro 404! Ops, a página não foi encontrada! </h1>
-            <Image src={erro} alt={""}></Image>
+        <div className="flex flex-col gap-1 p-8 justify-center items-center">
+            <h1 className="text-white font-black font-archivo text-3xl"> Erro 404! Ops, a página não foi encontrada! </h1>
+            <Image src={erro} width={900} height={450} alt={"Imagem de Erro!"}></Image>
         </div>
     )
 }
