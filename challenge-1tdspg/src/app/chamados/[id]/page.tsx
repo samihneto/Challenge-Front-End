1a4d44b296c8d081a0355c4fe3cb277145e89674
Samir Neto <samihneto@gmail.com>
2024-10-31 19:38:18 -0300

    Criação de rota dinâmica para Chamados.
    Co-authored-by: André M. <andremarko@users.noreply.github.com>
    Co-authored-by: Felipe_Fidelix <felipe-2833@users.noreply.github.com>

---
+++ challenge-1tdspg/src/app/chamados/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function Chamado(params: any) {
+  return (
+    <div>
+        <h1>Chamado {params.id}</h1>
+    </div>
+  )
+}
---
+++ challenge-1tdspg/src/app/chamados/page.tsx
@@ -0,0 +1,11 @@
+export default function Chamados() {
+    return (
+      <div>
+          <h1>Lista de Chamados</h1>
+          <ul>
+            
+          </ul>
+      </div>
+    )
+  }
+  
